feac5b6ec1dc2177e74b2c491115cccedb2d9328
rodrigo debian <correo.rodrigo@gmail.com>
2021-03-19 08:58:10 -0600

    progres

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ use crate::types::types::Input;
 use std::io::{self, BufRead};
 mod types;
 
+/*
+
+use crate::types::types::output_to_json;
+use crate::types::types::to_output;
+use crate::types::types::Output;
+
+*/
+
 fn main() {
     let stdin = io::stdin();
     let inputs: Vec<Input> = stdin.lock().lines().map(json_to_input).collect();
@@ -45,4 +53,12 @@ fn main() {
             year, dep, dep_acc, valor_neto
         );
     }
+
+    /*
+    let outputs: Vec<String> = to_output(item).iter().map(output_to_json).collect();
+
+    for o in outputs {
+        println!("{:?}", o);
+    }
+    */
 }
--- src/types.rs
+++ src/types.rs
@@ -13,6 +13,15 @@ pub mod types {
         },
     }
 
+    #[derive(Clone, Serialize, Deserialize, Debug)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Output {
+        pub year: i64,
+        pub depreciacion_anual: f64,
+        pub depreciacion_acumulada: f64,
+        pub valor_neto: f64,
+    }
+
     pub fn json_to_input(line: io::Result<String>) -> Input {
         let json = &line.unwrap();
         let deserialized: Input = serde_json::from_str(json).unwrap();
@@ -44,4 +53,40 @@ pub mod types {
             Input::TasaFijaUniforme { costo, .. } => costo.clone(),
         }
     }
+
+    /*
+    pub fn to_output(item: &Input) -> Vec<Output> {
+        let vida_util = years(item) as i64 + 1;
+        let mut valor_neto = cost(item);
+        let mut dep_acc = 0 as f64;
+        let mut result = Vec::new();
+        let tfu = tasa_fija_uniforme(item);
+        for year in 0..vida_util {
+            if year == 0 {
+                result.push(Output {
+                    year: year,
+                    depreciacion_anual: 0 as f64,
+                    depreciacion_acumulada: 0 as f64,
+                    valor_neto: valor_neto,
+                });
+                continue;
+            }
+            let dep = valor_neto * tfu;
+            dep_acc = dep_acc + dep;
+            valor_neto = valor_neto - dep;
+            result.push(Output {
+                year: year,
+                depreciacion_anual: dep,
+                depreciacion_acumulada: dep_acc,
+                valor_neto: valor_neto,
+            });
+        }
+
+        result
+    }
+
+    pub fn output_to_json(output: &Output) -> String {
+        serde_json::to_string(output).unwrap()
+    }
+    */
 }
